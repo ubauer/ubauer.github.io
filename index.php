43fe9d181a6b2a29d897a2f250bdbf6d328e67d9
Ulrich Bauer <ulrich.bauer@ist.ac.at>
2013-07-09 11:43:26 +0200

    initial checkin

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>Homepage of Ulrich Bauer</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<meta name="description" content="Ulrich Bauer, research assistant, Discrete Differential Geometry group, University of Göttingen">
+	<meta name="author" content="Ulrich Bauer">
+	<meta name="robots" content="follow">
+	<? $nav = 'Ulli'; $dir = 'http://ddg.math.uni-goettingen.de'; 
+	include 'include/head.inc'; ?>
+	
+</head>
+
+<body>
+	<? $dir = 'http://ddg.math.uni-goettingen.de'; 
+	include 'include/header.inc'; ?>
+	<div id="rumpf">
+		<div id="contentwrapper">
+			<div id="rechtespalte">
+				<div class="innen_content">
+					<br>
+					<font class="ueberschrift">Ulrich Bauer</font><br><br>
+					<p class="text">
+						I am a member of <a href="http://www.cs.duke.edu/~edels/">
+						Herbert Edelsbrunner's</a> research group at <A HREF="http://www.ist.ac.at">IST Austria</A>.
+						
+						I did my PhD with the research group <a href="http://ddg.math.uni-goettingen.de/">
+						Discrete Differential Geometry</a> at the University of G&ouml;ttingen.
+					</p>
+					<br><br>
+					
+					<strong>Research interests:</strong><br>
+					<p class="text">
+						Discrete/Computational Geometry/Topology, Algorithms, Combinatorics. 
+					</p>
+					<br><br>
+					
+					<strong>Contact:</strong><br>
+					<table border="0" cellpadding="0" cellspacing="0">
+					<tbody>
+						<tr>
+							<td valign="top" align="center"> <br></td>
+							<td valign="middle" width="300">
+								<A HREF="http://www.ist.ac.at">IST Austria</A><br />
+	Object 1, Floor 2<br />
+	Computer Science<br />
+	Am Campus 1<br />
+	3400 Klosterneuburg<br />
+	Austria
+
+							</td>
+							<td valign="middle" width="300">
+								<strong>Vox</strong>: +43 (0)2243 9000-3302<br>
+								<strong>Net</strong>: lastname (at) ist.ac.at							  										
+							</td>
+						</tr>
+					</tbody>
+					</table>
+					<br><br>
+					
+					<? include './include/publications.inc'; ?>
+					
+					
+		  			</div>
+			</div>
+		</div>
+		<div id="linkespalte">
+			<div class="innen"> 
+				<center>
+				<img src="http://ddg.math.uni-goettingen.de/img/ulli_bauer_bw.jpg"  
+				style="text-align: center; border: 0px solid;"  alt="That's me">
+				</center>
+	  		</div>
+		</div><br style="clear: both;">
+	</div>
+	<? include 'http://ddg.math.uni-goettingen.de/include/footer.inc'; ?>
+</body></html>
